5821c1772d63d263c1a470da8cd5264a80cee01c
daff00 <daffakpc21@gmail.com>
2026-05-12 12:04:17 +0700

    chore: scaffolding frontend project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-background">
+      <Button variant="default" className="shadow-lg">
+        Vite 8 + Tailwind v4 Works!
+      </Button>
+    </div>
+    </>
+  )
+}
+
+export default App
